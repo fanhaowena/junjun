776adf613c09db63b642b84ab2f0c42108f9a47e
fanhaowena <1612274972@qq.com>
2019-07-22 21:36:56 +0800

    create a file

---
+++ psbizwebb/psbizweb-endpoint/src/main/java/com/gaoyang/psbizweb/WebSpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.gaoyang.psbizweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSpringBootApplication {
+    public static void main(String[] args) throws Exception {
+
+        SpringApplication.run(WebSpringBootApplication.class, args);
+
+    }
+
+}
